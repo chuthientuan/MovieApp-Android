1023bbd88c207c3d77db7f7b42e72201e690fcfb
Tuan <tuanchu24112004@gmail.com>
2025-03-24 22:11:11 +0700

    Test create user

--- app/src/main/java/com/example/moviesapp/util/FirebaseUtil.java
+++ app/src/main/java/com/example/moviesapp/util/FirebaseUtil.java
@@ -1,4 +1,9 @@
 package com.example.moviesapp.util;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class FirebaseUtil {
+    public static String getCurrentUserId() {
+        return FirebaseAuth.getInstance().getCurrentUser().getUid();
+    }
 }
